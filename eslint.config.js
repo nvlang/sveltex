108cf5a79dded569253126e33b87706b8c8afc75
N. V. Lang <git@nvlang.dev>
2024-03-05 05:34:49 +0100

    build(lint): modernize ESLint config
    See https://github.com/eslint/rfcs/tree/main/designs/2019-config-simplification
    and https://eslint.org/blog/2023/10/flat-config-rollout-plans/.

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import eslint from '@eslint/js';
+import jestPlugin from 'eslint-plugin-jest';
+import prettierConfig from 'eslint-config-prettier';
+import tsdocPlugin from 'eslint-plugin-tsdoc';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: [
+            '.DS_Store',
+            'node_modules',
+            '/build',
+            '/package',
+            '.env',
+            '.env.*',
+            '!.env.example',
+            'pnpm-lock.yaml',
+            'package-lock.json',
+            'yarn.lock',
+        ],
+    },
+    {
+        // TypeScript files
+        files: ['**/*.ts'],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.stylisticTypeChecked,
+            ...tseslint.configs.strictTypeChecked,
+        ],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: 'tsconfig.json',
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            jest: jestPlugin,
+            tsdoc: tsdocPlugin,
+        },
+        rules: {
+            'tsdoc/syntax': 'warn',
+        },
+    },
+    {
+        // JavaScript files
+        files: ['**/*.js'],
+        ignores: ['**/eslint.config.js'],
+        ...tseslint.configs.disableTypeChecked,
+    },
+    {
+        // Jest test files
+        files: ['tests/**'],
+        ...jestPlugin.configs['flat/recommended'],
+    },
+    prettierConfig,
+);
