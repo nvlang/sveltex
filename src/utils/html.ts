a0fb6e3c9630ab51fa6f5b8897bf874384d28f40
N. V. Lang <git@nvlang.dev>
2024-05-06 18:25:36 +0200

    docs: document `isValidComponentName`

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -1,5 +1,20 @@
 import { htmlTagNames } from 'html-tag-names';
 
+/**
+ * Check if a string is a valid name for a component. For this to be the case,
+ * it has to not be an HTML tag name, and it has to be an alphanumeric string
+ * that starts with a letter.
+ *
+ * @param name - The name to check.
+ * @returns `true` if the name is valid, `false` otherwise.
+ *
+ * @example
+ * ```ts
+ * isValidComponentName('div'); // false
+ * isValidComponentName('div2'); // true
+ * isValidComponentName('MyComponent'); // true
+ * ```
+ */
 export function isValidComponentName(name: string): boolean {
     return /^[a-zA-Z][a-zA-Z0-9]*$/.test(name) && !htmlTagNames.includes(name);
 }
