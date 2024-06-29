277a9480c2887ff0e5abdb8a9a10ca5bda90adc6
N. V. Lang <git@nvlang.dev>
2024-06-30 01:34:23 +0200

    ci(e2e): give up on parallelization for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export const config = defineConfig({
     /* Retry on CI only */
     retries: process.env['CI'] ? 2 : 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: process.env['CI'] ? 'blob' : 'html',
+    reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
