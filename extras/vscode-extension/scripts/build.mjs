550edb2c8925fa98f2d1e6d98c7468a37153761d
N. V. Lang <git@nvlang.dev>
2024-06-30 06:47:58 +0200

    build(deps-dev): add `js-yaml` to vsc ext deps

--- extras/vscode-extension/scripts/build.mjs
+++ extras/vscode-extension/scripts/build.mjs
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import { load } from 'js-yaml';
 
 ['sveltex', 'markdown'].forEach((lang) => {
